81e68c8330c1c006ec563a102809b480f5c7652a
Racchel <racchelvelasco@gmail.com>
2022-04-03 18:24:45 -0300

    Move routes to own file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react'
 
-import {
-  Home,
-  About,
-  Movies,
-  Shows
-} from './pages'
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom'
+import { AppRoutes } from './routes'
 
 import { GlobalStyle } from './shared/styles'
 
@@ -22,26 +10,10 @@ class App extends Component {
     return (
       <>
         <GlobalStyle />
-        <Router>
-          <nav>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='about'>Sobre</Link></li>
-              <li><Link to='movies'>Filmes</Link></li>
-              <li><Link to='shows'>Séries</Link></li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/movies' element={<Movies />} />
-            <Route path='/shows' element={<Shows />} />
-          </Routes>
-        </Router>
+        <AppRoutes />
       </>
     )
   }
 }
 
-export default App;
+export default App
---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import {
+   Home,
+   About,
+   Movies,
+   Shows
+} from '../pages'
+
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Link
+} from 'react-router-dom'
+
+export const AppRoutes = () => {
+
+   return (
+      <>
+         <Router>
+            <nav>
+               <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='about'>Sobre</Link></li>
+                  <li><Link to='movies'>Filmes</Link></li>
+                  <li><Link to='shows'>Séries</Link></li>
+               </ul>
+            </nav>
+
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/about' element={<About />} />
+               <Route path='/movies' element={<Movies />} />
+               <Route path='/shows' element={<Shows />} />
+            </Routes>
+         </Router>
+      </>
+   )
+}
+
