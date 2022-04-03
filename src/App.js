21277de2fb01ba1888c5eb87012169a559bbcea6
Racchel <racchelvelasco@gmail.com>
2022-04-03 17:42:34 -0300

    Create global style - with reset

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react'
 
-import { 
-  Home, 
-  About, 
-  Movies, 
-  Shows 
+import {
+  Home,
+  About,
+  Movies,
+  Shows
 } from './pages'
 
 import {
-   BrowserRouter as Router,
-   Routes,
-   Route,
-   Link
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
 } from 'react-router-dom'
- 
+
+import { GlobalStyle } from './shared/styles'
 
 class App extends Component {
 
-  render () {
+  render() {
     return (
       <>
-      <Router>
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='about'>Sobre</Link></li>
-            <li><Link to='movies'>Filmes</Link></li>
-            <li><Link to='shows'>Séries</Link></li>
-          </ul>
-        </nav>
+        <GlobalStyle />
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='about'>Sobre</Link></li>
+              <li><Link to='movies'>Filmes</Link></li>
+              <li><Link to='shows'>Séries</Link></li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/movies' element={<Movies />}/>
-          <Route path='/shows' element={<Shows />}/>
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/shows' element={<Shows />} />
+          </Routes>
+        </Router>
       </>
     )
   }
