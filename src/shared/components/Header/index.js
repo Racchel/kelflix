c0e0e94cf37b50a082acbea832106a71ba9ffd15
Racchel <racchelvelasco@gmail.com>
2022-04-03 18:43:06 -0300

    Style header - in progress

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,23 +1,61 @@
 import React from 'react'
 
+/* styled-components */
+import styled from 'styled-components'
+
 /* react-router-dom */
 import {
    Link
 } from 'react-router-dom'
+
+const Container = styled.header`
+   width: 100%;
+   height: 60px;
+   display: flex;
+   justify-content: center;
+   background-color: #333;
+   color: #fff;
+   `
+
+const Nav = styled.header`
+   width: 80%;
+   height: 60px;
+   display: flex;
+   `
+
+const List = styled.ul`
+   width: 100%;
+   display: flex;
+   justify-content: center;
+   `
+
+const ListItem = styled.li`
+   width: 100px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   
+   &:hover {
+      background-color: #D81F26;
+      font-weight: bold;
+   }
+`
 
 /* Header */
 export const Header = () => {
 
    return (
       <>
-         <nav>
-            <ul>
-               <li><Link to='/'>Home</Link></li>
-               <li><Link to='about'>Sobre</Link></li>
-               <li><Link to='movies'>Filmes</Link></li>
-               <li><Link to='shows'>Séries</Link></li>
-            </ul>
-         </nav>
+         <Container>
+            <Nav>
+               <List>
+                  <ListItem><Link to='/'>Home</Link></ListItem>
+                  <ListItem><Link to='about'>Sobre</Link></ListItem>
+                  <ListItem><Link to='movies'>Filmes</Link></ListItem>
+                  <ListItem><Link to='shows'>Séries</Link></ListItem>
+               </List>
+            </Nav>
+         </Container>
       </>
    )
 }
--- src/shared/styles/globalStyle.js
+++ src/shared/styles/globalStyle.js
@@ -5,5 +5,13 @@ export const GlobalStyle = createGlobalStyle`
       margin: 0;
       padding: 0;
       box-sizing: border-box;
+      list-style: none;
+      text-decoration: none;
+      font-family: 'Courier New', Courier, monospace;
+   }
+
+   p, a {
+      font-size: 16px;
+      color: inherit;
    }
 `
