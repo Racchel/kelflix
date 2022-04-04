f6d6461c976061a3ba6b73fd03905373077ca506
Racchel <racchelvelasco@gmail.com>
2022-04-03 21:30:41 -0300

    Add object with breakpoints for responsiveness

---
+++ src/shared/styles/breakpoints.js
@@ -0,0 +1,12 @@
+export const breakpoints = {
+   /** max-width: 599px */
+   phone_only: '(max-width: 599px)',
+   /** min-width: 600px */
+   tablet_portrait_up: '(min-width: 600px)',
+   /** min-width: 900px */
+   tablet_landscape_up: '(min-width: 900px)',
+   /** min-width: 1200px */
+   desktop_up: '(min-width: 1200px)',
+   /** min-width: 1800px */
+   big_desktop_up: '(min-width: 1800px)'
+}
